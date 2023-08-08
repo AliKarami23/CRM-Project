aed234fa9d8947ecd858acd1135f00ff5167bb9f
MaryamHefazi <maryamhefazi@gmali.com>
2023-08-08 17:11:22 +0330

    Edit order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
-    protected $fillable = ['id','product' , 'price' , 'description' , 'buyer', 'created_at','updated_at	', 'status'];
+    protected $fillable = ['id','product_id' , 'price' , 'description' , 'user_id', 'created_at','updated_at'];
 }
