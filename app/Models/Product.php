0deac7c8be769d5ca9896c856a64c38f6d8b5f01
AliKarami23 <ali.karami.ali313@gmail.com>
2023-08-10 00:24:21 -0700

    fix Relationships in tabals

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
@@ -18,5 +19,10 @@ class Product extends Model
         'color' ,
         'image',
     ];
+
+    public function factor(): BelongsTo
+    {
+        return $this->belongsTo(Factor::class);
+    }
 
 }
