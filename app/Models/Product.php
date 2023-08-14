9c10a64ddeb23d43342349a2c008ff1d07125734
AliKarami23 <ali.karami.ali313@gmail.com>
2023-08-13 23:05:50 -0700

    add Relationships Many To Many in order and product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -27,4 +27,9 @@ class Product extends Model
         return $this->belongsTo(Factor::class);
     }
 
+    public function orders()
+    {
+        return $this->belongsToMany(Order::class);
+    }
+
 }
