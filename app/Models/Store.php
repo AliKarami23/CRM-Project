8d1896734cc820ee8887c0b716ba8c3dab8c0be5
AliKarami23 <ali.karami.ali313@gmail.com>
2023-10-31 16:23:14 +0330

    fix model store

--- app/Models/Store.php
+++ app/Models/Store.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Store extends Model
 {
     use HasFactory;
+
+
+    protected $fillable = [
+        'user_id',
+        'Location_lag',
+        'Location_lat',
+        'Status',
+        ];
 }
