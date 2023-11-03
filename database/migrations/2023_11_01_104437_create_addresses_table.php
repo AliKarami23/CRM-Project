8dfa5e05756e94a19cbd977ba83ed9427055a7f9
AliKarami23 <ali.karami.ali313@gmail.com>
2023-11-03 19:00:59 +0330

    test in host2

--- database/migrations/2023_11_01_104437_create_addresses_table.php
+++ database/migrations/2023_11_01_104437_create_addresses_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('Addresses', function (Blueprint $table) {
+        Schema::create('addresses', function (Blueprint $table) {
             $table->id();
             $table->string('user_id');
             $table->float('lag');
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('Addresses');
+        Schema::dropIfExists('addresses');
     }
 };
