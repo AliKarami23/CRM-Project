bdcfb4c8b862eb658c00313cb396e45b70986a18
AliKarami23 <ali.karami.ali313@gmail.com>
2023-11-06 10:53:28 +0330

    fix WebHookHandler.php in app file

--- app/Handler/WebHookHandler.php
+++ app/Handler/WebHookHandler.php
@@ -2,6 +2,11 @@
 
 namespace App\Handler;
 
+use App\Models\Factor;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\Store;
+use App\Models\User;
 use DefStudio\Telegraph\Handlers\EmptyWebhookHandler;
 use DefStudio\Telegraph\Keyboard\Button;
 use DefStudio\Telegraph\Keyboard\Keyboard;
@@ -17,6 +22,35 @@ class WebHookHandler extends EmptyWebhookHandler
 
     public function start(){
         $this->chat->message('start ok')->send();
+    }
+    public function admin(){
+        $adminCount = User::where('Role', 'Admin')->count();
+        $this->chat->message("تعداد مدیران: " . $adminCount)->send();
+    }
+
+    public function customer(){
+        $customerCount = User::where('Role', 'Customer')->count();
+        $this->chat->message("تعداد مشتریان: " . $customerCount)->send();
+    }
+
+    public function store(){
+        $storeCount = Store::count();
+        $this->chat->message("تعداد فروشگاه‌ها: " . $storeCount)->send();
+    }
+
+    public function order(){
+        $orderCount = Order::count();
+        $this->chat->message("تعداد سفارش‌ها: " . $orderCount)->send();
+    }
+
+    public function product(){
+        $productCount = Product::count();
+        $this->chat->message("تعداد محصولات: " . $productCount)->send();
+    }
+
+    public function factor(){
+        $factorCount = Factor::count();
+        $this->chat->message("تعداد فاکتورها: " . $factorCount)->send();
     }
 
 }
